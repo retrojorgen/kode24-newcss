77d7346ac55988d960887aa05f736329965d0d51
Jørgen Jacobsen <jorgeja@gmail.com>
2020-01-15 09:39:45 +0100

    toppbanner trycatch

--- src/kode24/topBanner.js
+++ src/kode24/topBanner.js
@@ -3,6 +3,16 @@ $(() => {
     return Math.floor(Math.random() * max + 0);
   }
 
+  function parseForBanners(data) {
+    try {
+      return JSON.parse(
+        data.result[0].content["lab-dz-1"][0].children[0].data.markup
+      );
+    } catch (error) {
+      return false;
+    }
+  }
+
   function getUrl(url, callback) {
     $.ajax({
       type: "GET",
@@ -17,63 +27,62 @@ $(() => {
   }
 
   getUrl("//api.kode24.no/front/?query=id:70559216", function(data) {
-    let config = JSON.parse(
-      data.result[0].content["lab-dz-1"][0].children[0].data.markup
-    );
+    let config = parseForBanners(data);
+    if (config) {
+      let ads = config;
+      let adNumber = randomNumber(ads.length);
+      let main = $("main");
+      let ad = ads[adNumber];
+      let desktopAd = ad.desktopBannerUrl;
+      let mobileAd = ad.mobileBannerUrl;
+      let mobileWidth = ad.mobileWidth || "";
+      let mobileHeight = ad.mobileHeight || "";
+      let desktopWidth = ad.desktopWidth || "";
+      let desktopHeight = ad.desktopHeight || "";
+      let url = ad.url;
+      let type = ad.type || "image";
+      let eventName = ad.eventName;
 
-    let ads = config;
-    let adNumber = randomNumber(ads.length);
-    let main = $("main");
-    let ad = ads[adNumber];
-    let desktopAd = ad.desktopBannerUrl;
-    let mobileAd = ad.mobileBannerUrl;
-    let mobileWidth = ad.mobileWidth || "";
-    let mobileHeight = ad.mobileHeight || "";
-    let desktopWidth = ad.desktopWidth || "";
-    let desktopHeight = ad.desktopHeight || "";
-    let url = ad.url;
-    let type = ad.type || "image";
-    let eventName = ad.eventName;
-
-    // Må endres hver gang
-    let campaignName = "bannerannonse kode24";
-    var AdElement = "";
-    if (type === "iframe") {
-      adElement = $(`
-        <div class="row top-profile" style="margin-top: 20px; max-width: ${desktopWidth};">
-          <div class="kicker">ANNONSE</div> 
-          <figure class="desktop">
-            <iframe src="${desktopAd}" frameborder="0" style="width:${desktopWidth}; height:${desktopHeight};"></iframe>
-          </figure>
-
-          <figure class="mobile">
-            <iframe src="${mobileAd}" frameborder="0" style="width:${mobileWidth}; height:${mobileHeight}"></iframe>
-          </figure>
-        </div>    
-      `);
-    } else {
-      adElement = $(`
-        <div class="row top-profile" style="margin-top: 20px;">
-          <a rel="noopener" itemprop="url" class="top-banner" href="${url}" target="_blank">
+      // Må endres hver gang
+      let campaignName = "bannerannonse kode24";
+      var AdElement = "";
+      if (type === "iframe") {
+        adElement = $(`
+          <div class="row top-profile" style="margin-top: 20px; max-width: ${desktopWidth};">
             <div class="kicker">ANNONSE</div> 
             <figure class="desktop">
-                <img itemprop="image" alt="annonse" src="${desktopAd}">
+              <iframe src="${desktopAd}" frameborder="0" style="width:${desktopWidth}; height:${desktopHeight};"></iframe>
             </figure>
 
             <figure class="mobile">
-                <img itemprop="image" alt="annonse" src="${mobileAd}">
+              <iframe src="${mobileAd}" frameborder="0" style="width:${mobileWidth}; height:${mobileHeight}"></iframe>
             </figure>
-          </a>
-        </div>    
-      `);
-    }
+          </div>    
+        `);
+      } else {
+        adElement = $(`
+          <div class="row top-profile" style="margin-top: 20px;">
+            <a rel="noopener" itemprop="url" class="top-banner" href="${url}" target="_blank">
+              <div class="kicker">ANNONSE</div> 
+              <figure class="desktop">
+                  <img itemprop="image" alt="annonse" src="${desktopAd}">
+              </figure>
 
-    if (!document.querySelector("header .full-bleed")) {
-      main.before(adElement);
-    }
+              <figure class="mobile">
+                  <img itemprop="image" alt="annonse" src="${mobileAd}">
+              </figure>
+            </a>
+          </div>    
+        `);
+      }
 
-    adElement.find("a").on("click", () => {
-      trackOutboundLink(campaignName, eventName, "klikk");
-    });
+      if (!document.querySelector("header .full-bleed")) {
+        main.before(adElement);
+      }
+
+      adElement.find("a").on("click", () => {
+        trackOutboundLink(campaignName, eventName, "klikk");
+      });
+    }
   });
 });
